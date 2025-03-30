4600a9dda1e36ce5665173d038743a0c3e90ebc8
Aykooo31 <ayhankosar95@gmail.com>
2025-03-29 20:58:08 -0700

    button, document.getElementById

--- Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -1,1 +1,7 @@
 alert("Hello,This is Ayhan!!");
+function My_First_Function(){
+		var String = "Smile, I am Turkish!";
+		var result = String.fontcolor("gray") ;
+		document.getElementById("Turkish").innerHTML=
+		result;
+	}
